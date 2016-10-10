47a62491b60353fb82c0acb4a817b760293554ea
wuzhenliang <wu@163.com>
2016-10-10 17:25:25 +0800

    初始化项目

---
+++ wechat/wechat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wechat
+//
+//  Created by wuzhengliang on 2016/10/10.
+//  Copyright © 2016年 com.bdxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wechat/wechat/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  wechat
+//
+//  Created by wuzhengliang on 2016/10/10.
+//  Copyright © 2016年 com.bdxy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"jiamengmeng");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
